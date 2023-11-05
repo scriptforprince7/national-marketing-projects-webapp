ce33b4ea97e39d4fe692a13c6a9562cf33c12859
Pushpraj Dwivedi <pushprajdwivedi001@g.ail.com>
2023-11-05 13:19:06 +0530

    separated header footer(php)

---
+++ includes/footer.php
@@ -0,0 +1,125 @@
+<footer class="footer-section">
+    <!--footer top start-->
+    <div class="footer-top gradient-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-9">
+                    <div class="row footer-top-wrap">
+                        <div class="col-md-4 col-sm-6">
+                            <div class="footer-nav-wrap text-white">
+                                <img src="img/logo-white.png" alt="logo" class="mb-2">
+                                <p>Continually myocardinate inexpensive catalysts for change with seamless
+                                    initiatives.</p>
+                                <div class="social-nav mt-4">
+                                    <ul class="list-unstyled social-list mb-0">
+                                        <li class="list-inline-item tooltip-hover">
+                                            <a href="#" class="rounded"><span class="ti-facebook"></span></a>
+                                            <div class="tooltip-item">Facebook</div>
+                                        </li>
+                                        <li class="list-inline-item tooltip-hover"><a href="#" class="rounded"><span
+                                                class="ti-twitter"></span></a>
+                                            <div class="tooltip-item">Twitter</div>
+                                        </li>
+                                        <li class="list-inline-item tooltip-hover"><a href="#" class="rounded"><span
+                                                class="ti-linkedin"></span></a>
+                                            <div class="tooltip-item">Linkedin</div>
+                                        </li>
+                                        <li class="list-inline-item tooltip-hover"><a href="#" class="rounded"><span
+                                                class="ti-dribbble"></span></a>
+                                            <div class="tooltip-item">Dribbble</div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-6">
+                            <div class="footer-nav-wrap text-white">
+                                <h4 class="text-white">COMPANY</h4>
+                                <ul class="nav flex-column">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">About Our Services</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Our Affiliates Program</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">View Our Blog</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Check Our Careers</a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-6">
+                            <div class="footer-nav-wrap text-white">
+                                <h4 class="text-white">LEGAL</h4>
+                                <ul class="nav flex-column">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Legal Information</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Privacy Policy</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Report Abuse</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Terms of Service</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-3">
+                    <div class="row footer-top-wrap mt-md-4 mt-sm-0 mt-lg-0">
+                        <div class="col-12">
+                            <div class="footer-nav-wrap text-white">
+                                <h4 class="text-white">GET IN TOUCH</h4>
+                                <ul class="get-in-touch-list">
+                                    <li class="d-flex align-items-center py-2"><span
+                                            class="fas fa-map-marker-alt mr-2"></span> 1234 Street Name, City Name, USA
+                                    </li>
+                                    <li class="d-flex align-items-center py-2"><span
+                                            class="fas fa-envelope mr-2"></span> you@domain.com
+                                    </li>
+                                    <!--<li class="d-flex align-items-center py-2"><span class="fas fa-phone-alt mr-2"></span> (123) 456-7890 - (123) 456-7890</li>-->
+                                </ul>
+                                <form class="newsletter-form mt-3">
+                                    <input type="text" class="input-newsletter" placeholder="Enter your email"
+                                           name="EMAIL" required="" autocomplete="off">
+                                    <button type="submit" class="disabled"
+                                            style="pointer-events: all; cursor: pointer;"><i
+                                            class="fas fa-paper-plane"></i></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--footer top end-->
+
+    <!--footer copyright start-->
+    <div class="footer-bottom gray-light-bg py-2">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-md-5 col-lg-5">
+                    <p class="copyright-text pb-0 mb-0">Copyrights © 2020. All
+                        rights reserved by
+                        <a href="https://themeforest.net/user/themetags" target="_blank">ThemeTags</a></p>
+                </div>
+                <div class="col-md-7 col-lg-6 d-none d-md-block d-lg-block">
+                    <ul class="list-inline policy-nav text-right social-list">
+                        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+                        <li class="list-inline-item"><a href="#">Terms & Conditions</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--footer copyright end-->
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,111 @@
+<header class="header">
+    <!--topbar start-->
+    <div id="header-top-bar" class="primary-bg py-2">
+        <div class="container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-md-7 col-lg-7 d-none d-md-block d-lg-block">
+                    <div class="topbar-text text-white">
+                        <ul class="list-inline">
+                            <li class="list-inline-item"><span class="fas fa-headphones-alt mr-1"></span> 24x7 Technical Support</li>
+                            <li class="list-inline-item"><span class="fas fa-phone-alt mr-1"></span> 883-4565-123456</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-4">
+                    <div class="topbar-text text-white">
+                        <ul class="list-inline text-md-right text-lg-right text-left  mb-0">
+                            <li class="list-inline-item">
+                                <a href="#" class="rounded"><span class="fab fa-facebook-f"></span></a>
+                            </li>
+                            <li class="list-inline-item"><a href="#" class="rounded"><span
+                                    class="fab fa-twitter"></span></a>
+                            </li>
+                            <li class="list-inline-item"><a href="#" class="rounded"><span
+                                    class="fab fa-linkedin-in"></span></a>
+                            </li>
+                            <li class="list-inline-item"><a href="#" class="rounded"><span
+                                    class="fab fa-dribbble"></span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--topbar end-->
+    <!--start navbar-->
+    <nav class="navbar navbar-expand-lg fixed-top white-bg">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">
+                <img src="img/logo-color.png" alt="logo" class="img-fluid"/>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="ti-menu"></span>
+            </button>
+            <div class="collapse navbar-collapse h-auto" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto menu">
+                    <li><a href="#" class="dropdown-toggle"> Home</a>
+                        <ul class="sub-menu">
+                            <li><a href="index.html">Home Page 01</a></li>
+                            <li><a href="index-2.html">Home Page 02</a></li>
+                            <li><a href="index-3.html">Home Page 03</a></li>
+                            <li><a href="index-4.html">Home Page 04</a></li>
+                            <li><a href="index-5.html">Home Page 05</a></li>
+                            <li><a href="index-6.html">Home Page 06</a></li>
+                            <li><a href="index-7.html">Home Page 07 <span class="badge badge-warning ml-2">New</span></a></li>
+                        </ul>
+                    </li>
+                    <li><a href="about-us.html">About Us</a></li>
+                    <li><a href="#" class="dropdown-toggle">Pages</a>
+                        <ul class="sub-menu">
+                            <li><a href="#" class="dropdown-toggle-inner">Login & Sign Up</a>
+                                <ul class="sub-menu">
+                                    <li><a href="basic-login.html">Login Page 1</a></li>
+                                    <li><a href="login.html">Login Page 2</a></li>
+                                    <li><a href="basic-sign-up.html">Signup Page 1</a></li>
+                                    <li><a href="sign-up.html">Signup Page 2</a></li>
+                                    <li><a href="password-reset.html">Reset Password</a></li>
+                                    <li><a href="change-password.html">Change Password</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#" class="dropdown-toggle-inner">Utilities</a>
+                                <ul class="sub-menu">
+                                    <li><a href="download.html">Download Page</a></li>
+                                    <li><a href="review.html">Review Page</a></li>
+                                    <li><a href="faq.html">FAQ Page</a></li>
+                                    <li><a href="404.html">404 Page</a></li>
+                                    <li><a href="coming-soon.html">Coming Soon</a></li>
+                                    <li><a href="thank-you.html">Thank You Page</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#" class="dropdown-toggle-inner">Team</a>
+                                <ul class="sub-menu">
+                                    <li><a href="team.html">Our Team Members</a></li>
+                                    <li><a href="team-single.html">Team Member Profile</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#" class="dropdown-toggle-inner">Our Blog</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog-default.html">Blog Grid</a></li>
+                                    <li><a href="blog-no-sidebar.html">Blog No Sidebar</a></li>
+                                    <li><a href="blog-left-sidebar.html">Blog Left Sidebar</a></li>
+                                    <li><a href="blog-right-sidebar.html">Blog Right Sidebar</a></li>
+                                    <li><a href="blog-right-sidebar-2.html">Blog Right Sidebar 02</a></li>
+                                    <li><a href="blog-single-left-sidebar.html">Details Left Sidebar</a></li>
+                                    <li><a href="blog-single-right-sidebar.html">Details Right Sidebar</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="project-details.html">Project Details </a></li>
+                            <li><a href="services-details.html">Services Details</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="project.html">Case Study</a></li>
+                    <li><a href="contact-us.html">Contact Us</a></li>
+                    <li><a href="#" class="btn secondary-solid-btn check-btn">Free Check</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
