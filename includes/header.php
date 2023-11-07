cbc1feb3fc2059ca72f8b33a6a4984218192a897
Pushpraj Dwivedi <pushprajdwivedi001@g.ail.com>
2023-11-07 18:36:47 +0530

    new-header

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,83 @@
+<link rel="stylesheet" href="../css/header.css">
 <header>
-    <div class="header-wrapper">
-        <div class="logo-wrapper">
-        </div>
-        <div class="nav-bar-wrapper">
-        </div>
+    <div class="container">
+        <div class="top-bar"></div>
+        <nav class="custom-navbar">
+            <div class="nav-wrapper">
+                <div class="logo">
+                    <img src="../img/logo.png" alt="">
+                </div>
+                <div class="nav-links">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Main Services</a>
+                            <ul class="nav-card">
+                                <li><a href="#">Service Category 1</a>
+                                    <ul>
+                                        <li><a href="#">Service 01</a></li>
+                                        <li><a href="#">Service 02</a></li>
+                                        <li><a href="#">Service 03</a></li>
+                                        <li><a href="#">Service 04</a></li>
+                                        <li><a href="#">Service 05</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Service Category 2</a>
+                                    <ul>
+                                        <li><a href="#">Service 06</a></li>
+                                        <li><a href="#">Service 07</a></li>
+                                        <li><a href="#">Service 08</a></li>
+                                        <li><a href="#">Service 09</a></li>
+                                        <li><a href="#">Service 10</a></li>
+                                    </ul>
+                                </li>
+                                <!-- Add more service categories here -->
+                            </ul>
+                        </li>
+                        <li><a href="#">Other Services</a>
+                            <ul class="nav-card">
+                                <li><a href="#">Service Category 3</a>
+                                    <ul>
+                                        <li><a href="#">Service 11</a></li>
+                                        <li><a href="#">Service 12</a></li>
+                                        <li><a href="#">Service 13</a></li>
+                                        <li><a href="#">Service 14</a></li>
+                                        <li><a href="#">Service 15</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Service Category 4</a>
+                                    <ul>
+                                        <li><a href="#">Service 16</a></li>
+                                        <li><a href="#">Service 17</a></li>
+                                        <li><a href="#">Service 18</a></li>
+                                        <li><a href="#">Service 19</a></li>
+                                        <li><a href="#">Service 20</a></li>
+                                    </ul>
+                                </li>
+                                <!-- Add more service categories here -->
+                            </ul>
+                        </li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="button">
+                    Login
+                </div>
+            </div>
+        </nav>
     </div>
 </header>
+
+<script>
+    // Add a script to close the dropdowns when clicking outside
+    // document.addEventListener('click', function(event) {
+    //     const dropdowns = document.querySelectorAll('.dropdown');
+
+    //     dropdowns.forEach(function(dropdown) {
+    //         if (!dropdown.contains(event.target)) {
+    //             const dropdownMenu = dropdown.querySelector('ul');
+    //             dropdownMenu.style.display = 'none';
+    //         }
+    //     });
+    // });
+</script>
