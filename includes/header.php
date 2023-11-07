04a76c9be59b2fc9e283a5859e726166d9b82000
Pushpraj Dwivedi <pushprajdwivedi001@g.ail.com>
2023-11-07 19:00:41 +0530

    updated header

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@
                         <li><a href="#">About</a></li>
                         <li><a href="#">Main Services</a>
                             <ul class="nav-card">
-                                <li><a href="#">Service Category 1</a>
+                                <li><a href="#">Service Category 01</a>
                                     <ul>
                                         <li><a href="#">Service 01</a></li>
                                         <li><a href="#">Service 02</a></li>
@@ -22,7 +22,7 @@
                                         <li><a href="#">Service 05</a></li>
                                     </ul>
                                 </li>
-                                <li><a href="#">Service Category 2</a>
+                                <li><a href="#">Service Category 02</a>
                                     <ul>
                                         <li><a href="#">Service 06</a></li>
                                         <li><a href="#">Service 07</a></li>
@@ -31,12 +31,7 @@
                                         <li><a href="#">Service 10</a></li>
                                     </ul>
                                 </li>
-                                <!-- Add more service categories here -->
-                            </ul>
-                        </li>
-                        <li><a href="#">Other Services</a>
-                            <ul class="nav-card">
-                                <li><a href="#">Service Category 3</a>
+                                <li><a href="#">Service Category 03</a>
                                     <ul>
                                         <li><a href="#">Service 11</a></li>
                                         <li><a href="#">Service 12</a></li>
@@ -45,13 +40,72 @@
                                         <li><a href="#">Service 15</a></li>
                                     </ul>
                                 </li>
-                                <li><a href="#">Service Category 4</a>
+                                <li><a href="#">Service Category 04</a>
                                     <ul>
                                         <li><a href="#">Service 16</a></li>
                                         <li><a href="#">Service 17</a></li>
                                         <li><a href="#">Service 18</a></li>
                                         <li><a href="#">Service 19</a></li>
                                         <li><a href="#">Service 20</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Service Category 05</a>
+                                    <ul>
+                                        <li><a href="#">Service 21</a></li>
+                                        <li><a href="#">Service 22</a></li>
+                                        <li><a href="#">Service 23</a></li>
+                                        <li><a href="#">Service 24</a></li>
+                                        <li><a href="#">Service 25</a></li>
+                                    </ul>
+                                </li>
+                                <!-- Add more service categories here -->
+                            </ul>
+                        </li>
+                        <li><a href="#">Other Services</a>
+                            <ul class="nav-card">
+                                <li><a href="#">Service Category 06</a>
+                                    <ul>
+                                        <li><a href="#">Service 26</a></li>
+                                        <li><a href="#">Service 27</a></li>
+                                        <li><a href="#">Service 28</a></li>
+                                        <li><a href="#">Service 29</a></li>
+                                        <li><a href="#">Service 30</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Service Category 07</a>
+                                    <ul>
+                                        <li><a href="#">Service 31</a></li>
+                                        <li><a href="#">Service 32</a></li>
+                                        <li><a href="#">Service 33</a></li>
+                                        <li><a href="#">Service 34</a></li>
+                                        <li><a href="#">Service 35</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Service Category 08</a>
+                                    <ul>
+                                        <li><a href="#">Service 36</a></li>
+                                        <li><a href="#">Service 37</a></li>
+                                        <li><a href="#">Service 38</a></li>
+                                        <li><a href="#">Service 39</a></li>
+                                        <li><a href="#">Service 40</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Service Category 09</a>
+                                    <ul>
+                                        <li><a href="#">Service 41</a></li>
+                                        <li><a href="#">Service 42</a></li>
+                                        <li><a href="#">Service 43</a></li>
+                                        <li><a href="#">Service 44</a></li>
+                                        <li><a href="#">Service 45</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Service Category 10</a>
+                                    <ul>
+                                        <li><a href="#">Service 46</a></li>
+                                        <li><a href="#">Service 47</a></li>
+                                        <li><a href="#">Service 48</a></li>
+                                        <li><a href="#">Service 49</a></li>
+                                        <li><a href="#">Service 50</a></li>
                                     </ul>
                                 </li>
                                 <!-- Add more service categories here -->
